4c584d3a59a18c04a81af4fe7f0b1fdfee629897
Galina Yakovenko <gyakovenko@gmail.com>
2017-02-21 09:22:55 -0800

    Update

---
+++ src/main/java/PetRegistration.java
@@ -0,0 +1,15 @@
+import com.sqa.gy.helpers.*;
+
+/**
+ * File Name: PetRegistration.java<br>
+ *
+ * Yakovenko, Galina<br>
+ * Created: Jan 18, 2017
+ */
+
+public class PetRegistration {
+
+	public static void main(String[] args) {
+		AppBasics.requestIntFromUser("Gimme an int:", 1, 100);
+	}
+}
---
+++ src/main/java/com/sqa/gy/helpers/exceptions/NumberNotInRangeException.java
@@ -0,0 +1,24 @@
+/**
+ *   File Name: NumberNotInRangeException.java<br>
+ *
+ *   Yakovenko, Galina<br>
+ *   Created: Jan 18, 2017
+ *
+ */
+
+package com.sqa.gy.helpers.exceptions;
+
+/**
+ * NumberNotInRangeException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Yakovenko, Galina
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class NumberNotInRangeException extends Exception {
+}
